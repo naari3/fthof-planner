824cc20f49de8e39606bb03ca059ceb5fbd6aaeb
naari3 <naari.named@gmail.com>
2020-12-20 06:14:04 +0900

    Install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,44 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
+      <h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
+      <p className="mb-2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        blanditiis consequatur eius hic ipsam nostrum omnis optio! Doloribus
+        quaerat quis ratione? At, maiores voluptas? Eveniet odio omnis
+        repellendus sapiente voluptatibus.
+      </p>
+      <button className="btn">a</button>
+      <table className="table-auto border-collapse w-full">
+        <thead>
+          <tr className="rounded-lg text-sm font-medium text-gray-700 text-left bg-gray-200">
+            <th className="px-4 py-2">Title</th>
+            <th className="px-4 py-2">Author</th>
+            <th className="px-4 py-2">Views</th>
+          </tr>
+        </thead>
+        <tbody className="text-sm font-normal text-gray-700">
+          <tr className="hover:bg-gray-100 border-b border-gray-200 py-10">
+            <td className="px-4 py-4">Intro to CSS</td>
+            <td className="px-4 py-4">Adam</td>
+            <td className="px-4 py-4">858</td>
+          </tr>
+          <tr className="hover:bg-gray-100 border-b border-gray-200 py-4">
+            <td className="px-4 py-4 flex items-center">
+              A Long and Winding Tour
+            </td>
+            <td className="px-4 py-4">Adam</td>
+            <td className="px-4 py-4">112</td>
+          </tr>
+          <tr className="hover:bg-gray-100  border-gray-200">
+            <td className="px-4 py-4">Intro to JavaScript</td>
+            <td className="px-4 py-4">Chris</td>
+            <td className="px-4 py-4">1,280</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
   );
 }
