3fd0c54a895637f66d2f3aa2ba2a2f06617dd0cc
naari3 <naari.named@gmail.com>
2021-09-04 22:34:21 +0900

    darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import { ThemeProvider as StyledComponentsThemeProvider } from "styled-components";
 import { ThemeProvider as MUIThemeProvider, StylesProvider } from "@mui/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, useMediaQuery } from "@mui/material";
 import "fontsource-roboto";
 
-import theme from "../styles/theme";
+// import theme from "../styles/theme";
 
 const MyApp = ({ Component, pageProps }): JSX.Element => {
   // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
@@ -15,6 +16,27 @@ const MyApp = ({ Component, pageProps }): JSX.Element => {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }, []);
+
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = useState(prefersDarkMode);
+  console.log(prefersDarkMode);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: darkMode ? "dark" : "light",
+        },
+      }),
+    []
+  );
+
+  useEffect(() => {
+    setDarkMode(prefersDarkMode);
+  }, [prefersDarkMode]);
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <StylesProvider injectFirst>
